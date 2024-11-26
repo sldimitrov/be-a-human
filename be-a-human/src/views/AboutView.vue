6bfa0d2abebcba8cebbd5d85b797ed9f5f812f2d
Slavi FE <slavi@wearealldigital.com>
2024-11-26 21:44:22 +0200

    Initialise VueProject with router, typescript, eslint, primevue

---
+++ be-a-human/src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Button from "primevue/button"
+
+</script>
+
+<template>
+
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
