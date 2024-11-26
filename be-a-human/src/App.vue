9974cfb27ee1c92bce21a9b01784667be235f803
Slavi FE <slavi@wearealldigital.com>
2024-11-26 22:25:33 +0200

    Ñfeat(start-up-page) add simple views and connect them via router

--- be-a-human/src/App.vue
+++ be-a-human/src/App.vue
@@ -5,81 +5,58 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="@/assets/logo-badi-chovек.jpg" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink class="link" to="/">Начало</RouterLink>
+        <RouterLink class="link" to="/cause">Каузата</RouterLink>
+        <RouterLink class="link" to="/events">Събития</RouterLink>
+        <RouterLink class="link" to="/joinus">Включи се</RouterLink>
+        <RouterLink class="link" to="/contacts">Контакти</RouterLink>
       </nav>
     </div>
   </header>
-
-  <RouterView />
+  <section>
+    <RouterView />
+  </section>
 </template>
 
 <style scoped>
 header {
-  line-height: 1.5;
-  max-height: 100vh;
+  display: flex;
+  width: 100%;
+  height: 150px;
+  background: #fff;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+img {
+  max-height: 100%;
+}
+
+.wrapper {
+  width: 100%;
 }
 
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  display: flex;
+  margin-left: 8rem;
+  width: 80%;
+  height: 100%;
+  justify-content: space-around;
+  align-items: center;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.link {
+  color: black;
+  font-size: 22px;
+  list-style: none;
+  text-decoration: none;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+section {
+  display: flex;
+  justify-content: center;
+  margin-top: 5rem;
 }
 </style>
---
+++ be-a-human/src/views/CauseView.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <h1>Cause</h1>
+</template>
+
+<style>
+</style>
