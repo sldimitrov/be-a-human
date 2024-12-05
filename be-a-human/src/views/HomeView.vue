15e2de33c67ae2a3b78e241a2f37434afe040479
Slavi FE <slavi@wearealldigital.com>
2024-12-05 16:26:01 +0200

    feat(home-page-design) enrich the view of the carousel to fit the screen

--- be-a-human/src/views/HomeView.vue
+++ be-a-human/src/views/HomeView.vue
@@ -3,19 +3,24 @@ import Carousel from 'primevue/carousel';
 
 const products = [
   {
-    image: "https://budichovek.com/wp-content/uploads/2024/10/fb-cover-my-fav-1536x864.png",
-    name: "Инициативата",
-    price: "датира от 1990"
+    image: "https://scontent-sof1-1.xx.fbcdn.net/v/t39.30808-6/440981091_747492197577799_8646004885718395676_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=833d8c&_nc_ohc=eHALgWHPkjAQ7kNvgEBnRUp&_nc_zt=23&_nc_ht=scontent-sof1-1.xx&_nc_gid=AqE54Us67WrqDPWZbExaFje&oh=00_AYDkwZxJjNcr0ppaHXweHUaEVi83bRY3s6W_UqTfHXZ_iQ&oe=67577239",
+    title: "Мисия и дейност",
+    description: "- За нас"
   },
   {
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Aytos_from_Hisarya.jpg/1200px-Aytos_from_Hisarya.jpg",
-    name: " Айтос- градът ни",
-    price: "Още текст"
+    image: "https://scontent-sof1-1.xx.fbcdn.net/v/t39.30808-6/440980516_747491537577865_6077673971683506232_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=833d8c&_nc_ohc=8GdsSbSgcY0Q7kNvgEHTN41&_nc_zt=23&_nc_ht=scontent-sof1-1.xx&_nc_gid=A9KQkH0IaXPxtH89FJupd7T&oh=00_AYA55bouhL88qtjabeV_mwYOPBHM9KsSPGnlbO165VDYag&oe=675793D7",
+    title: "Дарения и парньорство",
+    description: "- Как да помогнете"
   },
   {
-    image: "https://static.bnr.bg/gallery/e0/e016e71bf6f6338d9652159e69d0845e.jpeg",
-    name: "Центъра",
-    price: "се вижда"
+    image: "https://scontent-sof1-2.xx.fbcdn.net/v/t39.30808-6/438895044_741896671470685_7240449433516507971_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=833d8c&_nc_ohc=NOfBScAVAYgQ7kNvgGVRj7z&_nc_zt=23&_nc_ht=scontent-sof1-2.xx&_nc_gid=A0y_bGpbcY_qHG9-tNzfTs-&oh=00_AYDb3c2EcUrIi1eyEwS0MDL_j0SVloH_SE9ot9bqc6ZdBQ&oe=675799A9",
+    title: "Стани доброволец",
+    description: "- Как да помогнете"
+  },
+  {
+    image: "https://scontent-sof1-2.xx.fbcdn.net/v/t39.30808-6/440777441_747491637577855_7961571551442918387_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=833d8c&_nc_ohc=9NeWUQsp1OMQ7kNvgGWS4Rf&_nc_zt=23&_nc_ht=scontent-sof1-2.xx&_nc_gid=A6Roxr4t23I_zOsd9iijYAe&oh=00_AYBTR0lCg8426HGfxRxgZbeWEXHMUeUqGpi42CEpQmZhrA&oe=6757938B",
+    title: "Предстоящи събития",
+    description: "- Събития"
   }
 ];
 
@@ -45,55 +50,71 @@ const responsiveOptions = [
     :numScroll="1"
     :responsiveOptions="responsiveOptions"
     circular
-    :autoplayInterval="3000"
+    :autoplayInterval="4000"
     class="custom-carousel"
   >
     <template #item="slotProps">
       <div class="poster">
         <div class="poster-text">
-          <h2 class="mb-4 font-medium">{{ slotProps.data.name }}</h2>
-          <h2 class="font-semibold text-xl">{{ slotProps.data.price }}</h2>
+          <h2 class="description">{{ slotProps.data.description }}</h2>
+          <h2 class="title">{{ slotProps.data.title }}</h2>
         </div>
         <div class="carousel-item-container poster-image">
-          <img :src="slotProps.data.image" :alt="slotProps.data.name" class="carousel-image" />
+          <img :src="slotProps.data.image" :alt="slotProps.data.title" class="carousel-image" />
         </div>
       </div>
-
-
     </template>
   </Carousel>
 </template>
 
 <style scoped>
 .custom-carousel {
-  min-height: 450px;
+  min-height: 100vh; /* Full viewport height for the carousel */
 }
 
 .carousel-item-container {
   display: flex;
   justify-content: center;
-  align-items: start;
+  align-items: center;
   height: 100%;
+  width: 100%;
 }
 
 .carousel-image {
-  max-width: 100%;
-  max-height: 100%;
-  object-fit: contain;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 }
 
 .poster {
   position: relative;
   display: flex;
-  justify-content: center;
+  justify-content: start;
+  align-items: start;
+  width: 100%;
+  height: 100vh;
 }
 
 .poster-text {
-  font-family: "Arial Rounded MT Bold",serif;
-  font-size: 1.5rem;
+  color: gold;
   position: absolute;
   display: flex;
-  align-items: center;
+  align-items: flex-start;
   flex-direction: column;
+  margin: 5rem;
+}
+
+h2 {
+  margin: 0;
+}
+
+.description {
+  font-size: 2rem;
+}
+
+.title {
+  font-size: 4rem;
+  font-weight: bold;
+  font-family: Bahnschrift, sans-serif;
 }
 </style>
