f33bdd6e1a3ee0ef1e7afa53a3279812fa52e8d4
Slavi FE <slavi@wearealldigital.com>
2024-12-28 18:31:50 +0200

    feat(apex-charts) add charts and create reusable header component

---
+++ src/components/base/BaseHeader.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+}>()
+</script>
+
+<template>
+<div class="header">
+  <h1>Каузата</h1>
+</div>
+</template>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  background: #61CE70;
+}
+</style>
